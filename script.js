fff2109e0059ca99deef0a44bf34c14e58273dac
wasik05 <pawel@wasik05.pl>
2025-03-20 01:00:55 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector('.container');
+    setTimeout(() => {
+        container.style.opacity = 1;
+    }, 1000); // Opóźnienie 1s na "smaczek"
+});
