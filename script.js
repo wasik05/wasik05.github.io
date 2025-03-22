95a71c5b027f17bb6e025efb0887b1c2eb00c31b
wasik05 <pawel@wasik05.pl>
2025-03-23 00:00:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const container = document.querySelector('.container');
-    setTimeout(() => {
-        container.style.opacity = 1;
-    }, 1000); // Opóźnienie 1s na "smaczek"
-});
+// Ukrycie ekranu ładowania po 2 sekundach
+setTimeout(() => {
+    document.querySelector('.loading-screen').classList.add('fade-out');
+    document.querySelector('.container').classList.add('fade-in');
+    document.querySelector('.header').classList.remove('hidden'); // Usunięcie klasy hidden
+    document.querySelector('.header').classList.add('fade-in'); // Dodanie klasy fade-in
+}, 2000);
+
+// Funkcja do pobrania URL webhooka z pliku
+function fetchWebhookURL() {
+    return fetch('webhook.txt')
+        .then(response => response.text())
+        .then(url => url.trim());
+}
+
+// Funkcja wysyłająca wiadomość do webhooka Discorda
+function sendWebhookMessage(webhookURL) {
+    const message = {
+        embeds: [{
+            description: "Strona została odwiedzona!",
+            color: 16777215, // Biały kolor
+            thumbnail: {
+                url: 'https://wasik05.github.io/webhook-visit.qr'
+            },
+            footer: {
+                text: "wasik05.pl"
+            }
+        }]
+    };
+
+    fetch(webhookURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(message)
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Wiadomość wysłana pomyślnie.');
+        } else {
+            console.error('Nie udało się wysłać wiadomości.');
+        }
+    })
+    .catch(error => {
+        console.error('Błąd:', error);
+    });
+}
+
+// Pobranie URL webhooka i wysłanie wiadomości po załadowaniu strony
+window.onload = function() {
+    fetchWebhookURL().then(sendWebhookMessage);
+};
